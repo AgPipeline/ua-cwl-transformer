4094c42684e929945616083c862ed13e4e31fab6
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-10-24 17:05:40 -0700

    Added method

--- common-image/transformer_class.py
+++ common-image/transformer_class.py
@@ -62,6 +62,19 @@ class Transformer():
         """
         return configuration.TRANSFORMER_SENSOR
 
+    def generate_transformer_md() -> dict:
+        """Generates metadata about this transformer
+        Returns:
+            Returns the transformer metadata
+        """
+        md = {
+                'version': configuration.TRANSFORMER_VERSION,
+                'name': configuration.TRANSFORMER_NAME,
+                'author': configuration.AUTHOR_NAME,
+                'description': configuration.TRANSFORMER_DESCRIPTION,
+                'repository': {'repUrl': configuration.REPOSITORY}
+              }
+
     # pylint: disable=no-self-use
     def add_parameters(self, parser: argparse.ArgumentParser) -> None:
         """Adds processing parameters to existing parameters
