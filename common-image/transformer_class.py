234c38baba3094aa3bc38884621c8e7959b611b7
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-12-05 11:34:33 -0700

    Adding geo referenced image support

--- common-image/transformer_class.py
+++ common-image/transformer_class.py
@@ -9,6 +9,8 @@ from terrautils.metadata import get_terraref_metadata as tr_get_terraref_metadat
                                 get_season_and_experiment as tr_get_season_and_experiment, \
                                 get_extractor_metadata as tr_get_extractor_metadata
 from terrautils.sensors import Sensors
+from terrautils.imagefile import get_epsg as tr_get_epsg, \
+                                 image_get_geobounds as tr_image_get_geobounds
 import terrautils.lemnatec
 
 import configuration
@@ -45,12 +47,12 @@ class __internal__():
 class Transformer():
     """Generic class for supporting transformers
     """
-    #pylint: disable=unused-argument
     def __init__(self, **kwargs):
         """Performs initialization of class instance
         Arguments:
             kwargs: additional parameters passed in to Transformer
         """
+        # pylint: disable=unused-argument
         self.sensor = None
         self.args = None
 
@@ -66,12 +68,35 @@ class Transformer():
         """
         return configuration.TRANSFORMER_SENSOR
 
-    # pylint: disable=no-self-use
+    def get_image_file_epsg(self, source_path: str) -> str:
+        """Returns the EPSG of the georeferenced image file
+        Arguments:
+            source_path: the path to the image to load the EPSG code from
+        Return:
+            Returns the EPSG code loaded from the file. None is returned if there is a problem or the file
+            doesn't have an EPSG code
+        """
+        # pylint: disable=no-self-use
+        return tr_get_epsg(source_path)
+
+    def get_image_file_geobounds(self, source_path: str) -> list:
+        """Uses gdal functionality to retrieve rectilinear boundaries from the file
+        Args:
+            source_path(str): path of the file to get the boundaries from
+        Returns:
+            The upper-left and calculated lower-right boundaries of the image in a list upon success.
+            The values are returned in following order: min_y, max_y, min_x, max_x. A list of numpy.nan
+            is returned if the boundaries can't be determined
+        """
+        # pylint: disable=no-self-use
+        return tr_image_get_geobounds(source_path)
+
     def generate_transformer_md(self) -> dict:
         """Generates metadata about this transformer
         Returns:
             Returns the transformer metadata
         """
+        # pylint: disable=no-self-use
         return {
             'version': configuration.TRANSFORMER_VERSION,
             'name': configuration.TRANSFORMER_NAME,
@@ -80,25 +105,25 @@ class Transformer():
             'repository': {'repUrl': configuration.REPOSITORY}
         }
 
-    # pylint: disable=no-self-use
     def add_parameters(self, parser: argparse.ArgumentParser) -> None:
         """Adds processing parameters to existing parameters
         Arguments:
             parser: instance of argparse
         """
+        # pylint: disable=no-self-use
         parser.add_argument('--logging', '-l', nargs='?', default=os.getenv("LOGGING"),
                             help='file or url or logging configuration (default=None)')
 
         parser.epilog = configuration.TRANSFORMER_NAME + ' version ' + configuration.TRANSFORMER_VERSION + \
                         ' author ' + configuration.AUTHOR_NAME + ' ' + configuration.AUTHOR_EMAIL
 
-    #pylint: disable=no-self-use
     def get_transformer_params(self, args: argparse.Namespace, metadata: dict) -> dict:
         """Returns a parameter list for processing data
         Arguments:
             args: result of calling argparse.parse_args
             metadata: the loaded metadata
         """
+        # pylint: disable=no-self-use
         # Setup logging
         pyc_setup_logging(args.logging)
 
