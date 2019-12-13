40bf982c11bda50fc76add4ed95c611c0d305d1d
Sithyphus <jorgebarrios@email.arizona.edu>
2019-12-13 12:48:56 -0700

    Put the linters in the bash script

--- test-image/test-files/test_entry.sh
+++ test-image/test-files/test_entry.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pylint --rcfile=/home/extractor/Organization-info/pylint.rc /home/extractor/*.py
+pylint --rcfile=/home/extractor/Organization-info/pylint.rc /home/extractor/**/*.py
 python3 -m pytest -v
